42dc984c14b549bbc2e5e88cd1f1c05a33c2e60f
Emilie Henry <ehenry24@students.polytechnic.org>
2024-01-11 09:26:53 -0800

    Added statements to save a, b, and c values

--- QuadraticSolver.java
+++ QuadraticSolver.java
@@ -1,5 +1,13 @@
+import java.util.*;
 public class QuadraticSolver{
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         System.out.println("welcome to the Quadratic Solver!");
+        System.out.println("Enter the 'a' value:");
+        int a = input.nextInt();
+        System.out.println("Enter the 'b' value:");
+        int b = input.nextInt();
+        System.out.println("Enter the 'c' value:");
+        int c = input.nextInt();
     }
 }
