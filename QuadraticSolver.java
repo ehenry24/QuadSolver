9e9b2993a399778b1278860edf2df64aed7accda
Emilie Henry <ehenry24@students.polytechnic.org>
2024-01-12 14:34:03 -0800

    added more words to the welcome message

--- QuadraticSolver.java
+++ QuadraticSolver.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class QuadraticSolver{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("welcome to the Quadratic Solver!");
+        System.out.println("Hello! Welcome to the Quadratic Solver! The following program solves problems with the equation Ax^2+Bx+C");
         System.out.println("Enter the 'a' value:");
         double a = input.nextDouble();
         System.out.println("Enter the 'b' value:");
@@ -17,11 +17,11 @@ public class QuadraticSolver{
         if(discriminant > 0){
            xval2 = ((-b - Math.sqrt(discriminant)) / (2*a));
            xval1 = ((-b + Math.sqrt(discriminant)) / (2*a));
-           System.out.println("The solutions are:"+ xval1 +" and "+xval2);
+           System.out.println("The solutions are:"+ xval1 +" and "+ xval2);
 
         } else if (discriminant == 0){
             xval1 = ((-b)/(2*a));
-            System.out.println("The solution is:"+xval1);
+            System.out.println("The solution is:"+ xval1);
 
         } else if (discriminant < 0){
             System.out.println("No real solutions found :(");
